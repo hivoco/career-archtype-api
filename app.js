ebf1e164d8e885820a8863a58a94c810b4bae04c
Krishna-hivoco <krishna@hivoco.com>
2025-01-16 12:04:49 +0530

    change description

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ export const createApp = () => {
   app.use(cookieParser());
 
   app.use((req, res, next) => {
-    const allowedOrigins = ["http://localhost:8806", "http://localhost:3000"];
+    const allowedOrigins = ["http://localhost:8806", "http://localhost:5100"];
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
